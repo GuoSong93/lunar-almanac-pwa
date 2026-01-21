5b2cce43fa1324b0bbe50fbf5fd40f14751e76b4
GuoSong93 <guosong00000011@gmail.com>
2026-01-21 20:53:04 +0800

    优化

--- sw.js
+++ sw.js
@@ -1,9 +1,14 @@
-const CACHE_NAME = 'time-calendar-v2.0';
+// sw.js 开头的 ASSETS 数组
+const CACHE_NAME = 'time-calendar-v2.1';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  'https://cdn.jsdelivr.net/npm/lunar-javascript/lunar.js'
+  './icon.svg',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  'https://cdn.jsdelivr.net/npm/lunar-javascript/lunar.js',
+  'https://fonts.googleapis.com/css2?family=Oswald:wght@400;700&display=swap'
 ];
 
 self.addEventListener('install', e => {
