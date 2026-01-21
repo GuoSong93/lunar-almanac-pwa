46e52c18421ebb4f7268848d54ee8ecfa41d89a6
GuoSong93 <guosong00000011@gmail.com>
2026-01-21 20:46:33 +0800

    优化代码，更新V2.0

--- sw.js
+++ sw.js
@@ -1,23 +1,15 @@
-const CACHE_NAME = 'lunar-almanac-v2';
+const CACHE_NAME = 'time-calendar-v2.0';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  './icon.svg',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
   'https://cdn.jsdelivr.net/npm/lunar-javascript/lunar.js'
 ];
 
-self.addEventListener('install', (e) => {
+self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
 });
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))));
-});
-
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
